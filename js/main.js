f98c3d9d699090a1546840dc487a15816690ec85
rfdomingues98 <rfdomingues98@gmail.com>
2019-04-04 15:20:20 +0100

    Sound adjustments

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
-window.onload = function () {
-    document.getElementById("audio").play();
-}
+window.addEventListener('load', () => {
+    // noinspection JSUnresolvedVariable
+    let audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', '../AudioPlaceholder.mp3');
+    xhr.responseType = 'arraybuffer';
+    xhr.addEventListener('load', () => {
+        let playsound = (audioBuffer) => {
+            let source = audioCtx.createBufferSource();
+            source.buffer = audioBuffer;
+            source.connect(audioCtx.destination);
+            source.loop = false;
+            source.start();
+
+            setTimeout(function () {
+                let t = document.createElement('p');
+                t.appendChild(document.createTextNode((new Date()).toLocaleString() + ': Sound played'));
+                document.querySelector('.output').appendChild(t);
+                playsound(audioBuffer);
+            }, 1000 + Math.random() * 2500);
+        };
+
+        audioCtx.decodeAudioData(xhr.response).then(playsound);
+    });
+    xhr.send();
+});
